eb84c3093b0a6a15f675fd5b061aea88d2584e39
dahmadjid <dahmadjid@gmail.com>
2022-04-09 00:07:05 +0200

    config

---
+++ main/firebase_config.h
@@ -0,0 +1,13 @@
+// Wifi Credentials
+#define SSID "SSID"
+#define PASSWORD "PASSWORD"
+
+// Read readme.md to properly configure api key and authentication
+
+// create a new api key and add it here 
+#define API_KEY "API_KEY"
+// Copy your firebase real time database link here 
+#define DATABASE_URL "https://[DATABASE_LINK]-default-rtdb.europe-west1.firebasedatabase.app/"  
+
+#define USER_EMAIL "testemail@gmail.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "testpass"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
